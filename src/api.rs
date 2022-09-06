26d45dc964639509d66932dc3e041b32f97e76ad
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-09-06 14:47:47 -0400

    Basic api

---
+++ src/api.rs
@@ -0,0 +1,12 @@
+use reqwest::Error;
+
+pub async fn search_for_book(name: &str) -> Result<(), Error> {
+    let body = reqwest::get("https://www.googleapis.com/books/v1/volumes?q=".to_owned() + name)
+        .await?
+        .text()
+        .await?;
+    
+    println!("{}", body);
+
+    Ok(())
+}
